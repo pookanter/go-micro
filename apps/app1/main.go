4fde3e2df9f6c117cb31d031e58fd2ee1b35f47c
sippakorn-phuakpong <cloneltorrer@gmail.com>
2024-06-09 13:58:43 +0700

    set up docker for app1

--- apps/app1/main.go
+++ apps/app1/main.go
@@ -2,18 +2,26 @@ package main
 
 import (
 	"app1/handler"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	router := http.NewServeMux()
 	router.HandleFunc("/health", handler.HealthCheck)
 
+	port, err := os.LookupEnv("PORT")
+	if !err {
+		log.Fatal("PORT not set")
+	}
+
 	server := http.Server{
-		Addr:    ":8080",
+		Addr:    ":" + port,
 		Handler: router,
 	}
 
 	log.Fatal(server.ListenAndServe())
+	fmt.Println("App1 is running on port: ", port)
 }

700d1345e2d0ea924ce5455e445ab35b09766983
sippakorn-phuakpong <cloneltorrer@gmail.com>
2024-06-09 12:55:05 +0700

    app1

---
+++ apps/app1/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"go-micro/apps/app1/handler"
+	"log"
+	"net/http"
+)
+
+func main() {
+	router := http.NewServeMux()
+	router.HandleFunc("/health", handler.HealthCheck)
+
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	log.Fatal(server.ListenAndServe())
+}

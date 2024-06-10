bd835e8b234abec30acc173838238def3ec6c45c
sippakorn-phuakpong <cloneltorrer@gmail.com>
2024-06-10 22:42:00 +0700

    save

--- apps/app1/handler/handler.go
+++ apps/app1/handler/handler.go
@@ -1,8 +1,21 @@
 package handler
 
-import "net/http"
+import (
+	"net/http"
+)
 
-func HealthCheck(w http.ResponseWriter, r *http.Request) {
+func HealthCheck(w http.ResponseWriter, req *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("App1, OK"))
+}
+
+func GetAuthor(w http.ResponseWriter, req *http.Request) {
+	// ctx, err := context.WithTimeout(req.Context(), time.Minute*2)
+	// if err != nil {
+	// 	w.WriteHeader(http.StatusInternalServerError)
+	// 	w.Write([]byte("Error creating context"))
+	// 	return
+	// }
+
+	// db.New(db.DB).GetAuthor(ctx, 1)
 }
